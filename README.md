ee593b3b22e56dfdf78e65afe796a1263b7734f9
Oleksandr Berchenko <ar.ukraine@gmail.com>
2021-02-03 15:48:34 +0200

    Add setup scripts

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-T.B.D.
+**Setup**
+
+1. Clone this repo to a local folder.
+
+2. Run `setup.bat` (Windows only) to generate the `.seext` files.  If needed, a Mac version will be added too.
+
+3. Install Strange Eons from https://strangeeons.cgjennings.ca/download.html, choose `Build 3970`.
+
+4. Run Strange Eons and install `The Lord of the Rings LCG` plugin.
+
+5. Go to plugins folder (run Strange Eons, then click `Toolbox` -> `Manage Plug-ins` -> `Open Plug-in Folder`),
+close Strange Eons and delete or move all files, which names start with `TheLordOfTheRingsLCG`.  Instead of them,
+copy all generated `.seext` files from the root folder of this repo.
---
+++ setup.bat
@@ -0,0 +1,18 @@
+del TheLordOfTheRingsLCG*.zip
+del TheLordOfTheRingsLCG*.seext
+
+CScript zip.vbs TheLordOfTheRingsLCG TheLordOfTheRingsLCG.zip
+CScript zip.vbs TheLordOfTheRingsLCG-A TheLordOfTheRingsLCG-A.zip
+CScript zip.vbs TheLordOfTheRingsLCG-B TheLordOfTheRingsLCG-B.zip
+CScript zip.vbs TheLordOfTheRingsLCG-E TheLordOfTheRingsLCG-E.zip
+CScript zip.vbs TheLordOfTheRingsLCG-I TheLordOfTheRingsLCG-I.zip
+CScript zip.vbs TheLordOfTheRingsLCG-P TheLordOfTheRingsLCG-P.zip
+CScript zip.vbs TheLordOfTheRingsLCG-V TheLordOfTheRingsLCG-V.zip
+
+ren TheLordOfTheRingsLCG.zip TheLordOfTheRingsLCG.seext
+ren TheLordOfTheRingsLCG-A.zip TheLordOfTheRingsLCG-A.seext
+ren TheLordOfTheRingsLCG-B.zip TheLordOfTheRingsLCG-B.seext
+ren TheLordOfTheRingsLCG-E.zip TheLordOfTheRingsLCG-E.seext
+ren TheLordOfTheRingsLCG-I.zip TheLordOfTheRingsLCG-I.seext
+ren TheLordOfTheRingsLCG-P.zip TheLordOfTheRingsLCG-P.seext
+ren TheLordOfTheRingsLCG-V.zip TheLordOfTheRingsLCG-V.seext
